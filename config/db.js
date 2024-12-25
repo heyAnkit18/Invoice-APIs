5d21864d396d167f5c50f77c8b2de9fe9a8ea929
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-12-25 22:50:49 +0530

    MongoDB Database Connected

---
+++ config/db.js
@@ -0,0 +1,15 @@
+require('dotenv').config({ path: '../.env' });
+
+// require('dotenv').config();
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Database connected');
+  } catch (err) {
+    console.error('Database connection error:', err.message);
+    process.exit(1);
+  }
+};
+connectDB();
