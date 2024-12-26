4f471463e27afe38127f019d0a0b538e812847dc
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-12-26 13:30:12 +0530

    Update server port to resolve port conflict

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.use('/api/invoices', require('./routes/invoice'));
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
