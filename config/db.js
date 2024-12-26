59e31452325999760a5b190576b8e74ce5e638c8
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-12-26 12:41:05 +0530

    Appjs Added, Db.js updated, Nodemon Added

--- config/db.js
+++ config/db.js
@@ -1,15 +1,13 @@
-require('dotenv').config({ path: '../.env' });
-
-// require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('Database connected');
+    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log('MongoDB Connected...');
   } catch (err) {
-    console.error('Database connection error:', err.message);
+    console.error(err.message);
     process.exit(1);
   }
 };
-connectDB();
+
+module.exports = connectDB;
