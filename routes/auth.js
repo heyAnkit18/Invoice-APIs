d48c45cc788d0487069c2f335fb7c798dc01f1c4
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-12-26 13:16:40 +0530

    auth.js added in routes

---
+++ routes/auth.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+
+const router = express.Router();
+
+router.post('/register', async (req, res) => {
+  const { username, password, role } = req.body;
+  const user = new User({ username, password, role });
+  await user.save();
+  res.send('User registered successfully');
+});
+
+router.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(400).send('Invalid Credentials');
+  }
+
+  const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET);
+  res.send({ token });
+});
+
+module.exports = router;
