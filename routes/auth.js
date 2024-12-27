339cf3ef455a7e0242ae921ec1504075977e8fe6
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-12-27 12:18:26 +0530

    auth.js updated in routes

--- routes/auth.js
+++ routes/auth.js
@@ -5,24 +5,27 @@ const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
+// Register
 router.post('/register', async (req, res) => {
   const { username, password, role } = req.body;
+
   const user = new User({ username, password, role });
   await user.save();
   res.send('User registered successfully');
 });
 
+// Login
 router.post('/login', async (req, res) => {
   const { username, password } = req.body;
+
   const user = await User.findOne({ username });
   if (!user || !(await bcrypt.compare(password, user.password))) {
     return res.status(400).send('Invalid Credentials');
   }
 
   const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET);
-  res.send({ token });
-  // console.log('JWT_SECRET:', process.env.JWT_SECRET);
-
+  res.json({ token });
 });
 
 module.exports = router;
+
